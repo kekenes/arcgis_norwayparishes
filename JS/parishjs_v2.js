be81ef901a7803a3be4c2ee3060a0f6125a80e9f
Kristian Ekenes <kekenes@esri.com>
2015-04-15 00:10:06 -0700

    Fix legend. Start popups

--- JS/parishjs_v2.js
+++ JS/parishjs_v2.js
@@ -87,6 +87,43 @@ require([
     
   //////////////////////END LAYERS AND LAYER INFO///////////////////////////
     
+    /////////////////////////POPUPs///////////////////////////////////
+ function setCountyInfo(info){
+    var countyName = info.COUNTY;
+    var wikiURL = info.WIKI;
+    var FSwiki = info.FS_WIKI;
+    var oldName = info.OLD_COUNTY;   //not in FS
+    var farmList = info.FARMS;      //not in FS
+ }
+    
+ function setMunicipalityInfo(info){
+    var municipalityName = info.MUNICIPALITY;
+    var countyName = info.COUNTY;
+ }
+
+ function setParishInfo(info){
+    var parishName = info.PAR_NAME;
+    var municipalityName = info.MUNICIPALITY;
+    var countyName = info.COUNTY;
+    var photoURL = info.PHOTO;
+    var photoOr = info.PHOTO_O;
+    var FSwiki = info.FAM_SEARCH;
+    var farmList = info.FARMS;
+    var churchURL = info.CHURCH;
+    var da1_url = info.DA_1 || null;
+    var da1_name = info.DA_1_NAME || null;
+    var da2_url = info.DA_2 || null;
+    var da2_name = info.DA_2_NAME || null;
+    var da3_url = info.DA_3 || null;
+    var da3_name = info.DA_3_NAME || null;
+    var da4_url = info.DA_4 || null;
+    var da4_name = info.DA_4_NAME || null;
+    var da5_url = info.DA_5 || null;
+    var da5_name = info.DA_5_NAME || null;
+ }
+    
+    ////////////////////////END POPUPS//////////////////////////////////
+    
   /////////////////////MAP AND LEGEND///////////////////////////////////////
     
   on(countiesLayer, "load", initMap);
@@ -94,20 +131,18 @@ require([
   function initMap(){
       
     map = new Map("map", {
-      extent: countiesLayer.fullExtent
+      extent: countiesLayer.fullExtent//,
+//      basemap: "topo"
     });
       
     console.log("map extent: ", map.extent);
       
-    map.addLayers([topoLayer, norTopoLayer, parishesLayer, municipalitiesLayer, countiesLayer]);
+    map.addLayers([satelliteLayer, topoLayer, norTopoLayer, streetLayer, parishesLayer, municipalitiesLayer, countiesLayer]);
+      satelliteLayer.hide();
+      streetLayer.hide();
       
     on(map, "layers-add-result", function(evt){
-//        var legend = new Legend({
-//          map: map
-//        }, "legendContent");
-//        
-//        legend.startup();
-        
+
         var toc = new TOC({
           map: map,
           layerInfos: [
@@ -122,73 +157,84 @@ require([
               {
                 layer: parishesLayer,
                 title: "Parishes"
-              },
-              {
-                layer: norTopoLayer,
-                title: "Norwegian Topographic Map",
-                slider: true
-              },
-              {
-                layer: streetLayer,
-                title: "Roads",
-                collapsed: true
-              },
-              {
-                layer: satelliteLayer,
-                title: "Satellite Imagery",
-                collapsed: true
-              },
-              {
-                layer: topoLayer,
-                title: "World Topographic Map",
-                collapsed: true
-              }
-              
+              }              
           ]
         }, "legendContent");
         toc.startup();
     });
       
-  }  
+  }
+    
+  on(document.getElementsByName("baseLayers")[1], "click", function(evt){
+    if(dom.byId("radioSat").checked){
+//        map.setBasemap("satellite");
+        satelliteLayer.show();
+        streetLayer.show();
+        topoLayer.hide();
+        norTopoLayer.hide();
+    }
+  });
+    
+  on(document.getElementsByName("baseLayers")[0], "click", function(evt){
+    if(dom.byId("radioTopo").checked){
+//        map.setBasemap("topo");
+        satelliteLayer.hide();
+        streetLayer.hide();
+        topoLayer.show();
+        norTopoLayer.show();
+    }
+  });
+    
+  on(dom.byId("streetCheck"), "click", function(evt){
+    if(dom.byId("streetCheck").checked){
+        streetLayer.show();
+    }
+    else{
+        streetLayer.hide();
+    }
+  });    
+    
+  
   
   ////////////////////END MAP AND LEGEND///////////////////////////
 
     ///////////////////////////EVENTS/////////////
+    dom.byId("legend").style.height = "35%";
+    dom.byId("tools").style.height = "50%";
     
     on(dom.byId("toolsHeader"), "click", function(){
         
-      if(dom.byId("tools").style.height == "50%"){    
+      if(dom.byId("tools").style.height == "50%"){  
           dom.byId("tools").style.height = "25px";
           dom.byId("toolsMinIcon").style.visibility = "hidden";
           dom.byId("toolsMaxIcon").style.visibility = "visible";
+          dom.byId("toolsContent").style.visibility = "hidden";
       }
       else{
           dom.byId("tools").style.height = "50%";
           dom.byId("toolsMinIcon").style.visibility = "visible";
           dom.byId("toolsMaxIcon").style.visibility = "hidden";
+          dom.byId("toolsContent").style.visibility = "visible";
       }
     });
     
     on(dom.byId("legendHeader"), "click", function(){
         
-      if(dom.byId("legend").style.height == "50%"){    
+      if(dom.byId("legend").style.height == "35%"){    
           dom.byId("legend").style.height = "25px";
           dom.byId("legendMinIcon").style.visibility = "hidden";
           dom.byId("legendMaxIcon").style.visibility = "visible";
+          dom.byId("legendContent").style.visibility = "hidden";
+          dom.byId("legendContentBase").style.visibility = "hidden";
+          
       }
       else{
-          dom.byId("legend").style.height = "50%";
+          dom.byId("legend").style.height = "35%";
           dom.byId("legendMinIcon").style.visibility = "visible";
           dom.byId("legendMaxIcon").style.visibility = "hidden";
+          dom.byId("legendContent").style.visibility = "visible";
+          dom.byId("legendContentBase").style.visibility = "visible";
       }
     });
-    
-//    on(dom.byId("legendHeader"), "click", function(){
-//      dom.byId("tools").style.height = "50%";
-//      dom.byId("toolsMinIcon").style.visibility = "visible";
-//      dom.byId("toolsMaxIcon").style.visibility = "hidden";
-//    });
-  
-  
    
 });
