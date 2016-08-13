a7206916a16c7d8d4e770e6b391bbc4f928dea2a
Kristian Ekenes <KEkenes@esri.com>
2016-08-13 00:52:55 -0700

    Move layer titles to constants. Clean up test page

--- JS/Modules/constants.js
+++ JS/Modules/constants.js
@@ -13,7 +13,12 @@ define([
     parishdd: dom.byId("prestegjeld-select"),
     localparishdd: dom.byId("sokn-select"),
     year: year,
-    defExp: "BEGIN_ <= " + year + " AND END_ > " + year
+    defExp: "BEGIN_ <= " + year + " AND END_ > " + year,
+
+    countyTitle: "Norge_boundaries - fylke",
+    cityTitle: "Norge_boundaries - herred",
+    parishTitle: "Norge_boundaries - prestegjeld",
+    localParishTitle: "Norge_boundaries - sokn"
   };
 
 });
