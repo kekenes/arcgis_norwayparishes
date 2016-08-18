0ff12ff780e80858bcf648c11c811571a98e223d
Kristian Ekenes <KEkenes@esri.com>
2016-08-17 23:35:04 -0700

    ensure year is current value of input

--- JS/Modules/constants.js
+++ JS/Modules/constants.js
@@ -5,15 +5,15 @@ define([
   dom
 ){
 
-  var year = dom.byId("year").value;
+  var year = dom.byId("year");
 
   return {
     countydd: dom.byId("fylke-select"),
     citydd: dom.byId("herred-select"),
     parishdd: dom.byId("prestegjeld-select"),
     localparishdd: dom.byId("sokn-select"),
-    year: year,
-    defExp: "BEGIN_ <= " + year + " AND END_ > " + year,
+    year: year.value,
+    defExp: "BEGIN_ <= " + year.value + " AND END_ > " + year.value,
 
     countyTitle: "Norge_boundaries - fylke",
     cityTitle: "Norge_boundaries - herred",
